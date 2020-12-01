24c32a608c6f9657ef06ae058341a4931bc5b3c6
Fluffas <kantabanga@gmail.com>
2020-12-01 08:47:39 +0100

    day1 part 1

---
+++ day1/day1.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.argv[1]
+
+with open(input, "r") as x:
+    for line1 in x:
+        num1 = int(line1.strip())
+
+        with open(input, "r") as y:
+            for line2 in y:
+                num2 = int(line2.strip())
+
+                if num1 != num2 and num1 + num2 == 2020:
+                    print(num1 * num2)
+                    sys.exit()
