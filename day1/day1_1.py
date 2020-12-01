53d04cec82cc682ac47a9a2a98cff91a19684a24
Fluffas <kantabanga@gmail.com>
2020-12-01 18:19:48 +0100

    idk

--- day1/day1_1.py
+++ day1/day1_1.py
@@ -1,13 +1,9 @@
 import sys
 with open(sys.argv[1], "r") as file:
-    input = file.readlines()
+    input = [int(i) for i in file.readlines()]
 
     for x in input:
-        num1 = int(x.strip())
-
         for y in input:
-            num2 = int(y.strip())
-
-            if num1 + num2 == 2020:
-                print(num1 * num2)
-                sys.exit("Success")
+            if x + y == 2020:
+                print(x * y)
+                sys.exit()
--- day1/day1_2.py
+++ day1/day1_2.py
@@ -1,16 +1,12 @@
 import sys
 with open(sys.argv[1], "r") as file:
-    input = file.readlines()
+    input = [int(i) for i in file.readlines()]
 
     for x in input:
-        num1 = int(x.strip())
-
         for y in input:
-            num2 = int(y.strip())
-
+            if x + y >= 2020:
+                continue
             for z in input:
-                num3 = int(z.strip())
-
-                if num1 + num2 + num3 == 2020:
-                    print(num1 * num2 * num3)
-                    sys.exit("Success")
+                if x + y + z == 2020:
+                    print(x * y * z)
+                    sys.exit()
