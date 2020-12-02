456ee187be190ac25852a89da10f267176785dd6
Fluffas <kantabanga@gmail.com>
2020-12-02 15:51:59 +0100

    day2_1 now with count

--- day2/day2_1.py
+++ day2/day2_1.py
@@ -11,11 +11,7 @@ with open(sys.argv[1], "r") as file:
         letter = split[0][len(split[0])-1]
         pw = split[1]
 
-        counter = 0
-        for match in re.finditer(letter, pw):
-            counter += 1
-
-        if vals[0] <= counter <= vals[1]:
+        if vals[0] <= pw.count(letter) <= vals[1]:
             valid += 1
 
 print(valid)
